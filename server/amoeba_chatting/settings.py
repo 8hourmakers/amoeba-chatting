e89e0312b909bdcee07cdbb798a92264fbef41e6
bdh1011 <bdh931101@gmail.com>
2017-05-14 01:36:28 +0900

    remove staticfile_dirs

--- server/amoeba_chatting/settings.py
+++ server/amoeba_chatting/settings.py
@@ -147,9 +147,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/amoeba_chatting/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "server", "static")
 
 MEDIA_URL = "/media/"
