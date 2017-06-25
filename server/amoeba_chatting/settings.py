76559a0e64e1a2e0f0b2010dba593deebd7ac12e
bdh1011 <bdh931101@gmail.com>
2017-06-25 11:57:28 +0900

    Update sis db endpoint as production db

--- server/amoeba_chatting/settings.py
+++ server/amoeba_chatting/settings.py
@@ -76,16 +76,27 @@ WSGI_APPLICATION = 'amoeba_chatting.wsgi.application'
 APPEND_SLASH = True
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     },
+#     'options': {
+#         'managed': True
+#     }
+# }
+# Database
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'options': {
-        'managed': True
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sis',
+        'USER': 'davidstock',
+        'PASSWORD': 'broker77',
+        'HOST': 'davidstock.cdfrphglbreq.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
 
 
 REST_FRAMEWORK = {
